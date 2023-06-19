935c1a7bbc0f779e8ff89994fc746005d14cb9cf
phuc.nguyen@inventis.vn <phuc.nguyen@inventis.vn>
2023-06-19 17:18:53 +0700

    show dialog, botttom sheet without context

--- lib/pages/home/home_view.dart
+++ lib/pages/home/home_view.dart
@@ -9,14 +9,79 @@ class HomeView extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: AppBar(title: const Text('Home')),
-        body: Center(
-          child: ElevatedButton(
-            onPressed: () {
-              // Navigator.pop(context);
-              Get.back();
-            },
-            child: const Text('Back to Login'),
+        body: Builder(builder: (context) {
+          return Center(
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                ElevatedButton(
+                  onPressed: () {
+                    // Navigator.pop(context);
+                    Get.back();
+                  },
+                  child: const Text('Back to Login'),
+                ),
+                const SizedBox(height: 8),
+                ElevatedButton(
+                  onPressed: () {
+                    showDialog(
+                        context: context, builder: (ctx) => _buildDialog(ctx: ctx));
+                    // Get.dialog(_buildDialog());
+                  },
+                  child: const Text('Show dialog'),
+                ),
+                const SizedBox(height: 8),
+                ElevatedButton(
+                  onPressed: () {
+                    showModalBottomSheet(
+                        context: context,
+                        builder: (ctx) => _buildBottomSheet(ctx: ctx));
+                    // Get.bottomSheet(_buildBottomSheet());
+                  },
+                  child: const Text('Show bottom sheet'),
+                ),
+              ],
+            ),
+          );
+        }));
+  }
+
+  Container _buildBottomSheet({BuildContext? ctx}) {
+    return Container(
+        color: Colors.white,
+        child: Center(
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              const Text('Hello, this is bottom sheet'),
+              ElevatedButton(
+                  onPressed: () {
+                    Navigator.pop(ctx!);
+                    // Get.back();
+                  },
+                  child: const Text('Close'))
+            ],
           ),
         ));
+  }
+
+  Widget _buildDialog({BuildContext? ctx}) {
+    return Dialog(
+      insetPadding: const EdgeInsets.all(64),
+      child: Center(
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            const Text('Hello, this is dialog'),
+            ElevatedButton(
+                onPressed: () {
+                  Navigator.pop(ctx!);
+                  // Get.back();
+                },
+                child: const Text('Close dialog'))
+          ],
+        ),
+      ),
+    );
   }
 }
