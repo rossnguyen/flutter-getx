05145a7295405f00e51fba40456f56dd17d96be1
phuc.nguyen@inventis.vn <phuc.nguyen@inventis.vn>
2023-06-20 15:48:53 +0700

    Worker: once, ever, debouce, interval

--- lib/pages/home/controllers/home_controller.dart
+++ lib/pages/home/controllers/home_controller.dart
@@ -1,17 +1,43 @@
+import 'package:flutter/cupertino.dart';
 import 'package:get/get.dart';
 
 class HomeController extends GetxController {
   final count = 0.obs;
+  final everMessage = ''.obs;
+  final onceMessage = ''.obs;
+  late Worker onceWorker, everWorker, debounceWorker, intervalWorker;
+  late TextEditingController inputController;
+  final inputText = ''.obs;
+  final RxList<String> debounceKeywordList = RxList();
+  final RxList<String> intervalKeywordList = RxList();
   @override
   void onInit() {
+    onceWorker = once<int>(count,
+        (value) => onceMessage.value = 'Once - Just update count to $value');
+    everWorker = ever<int>(count,
+        (value) => everMessage.value = 'Ever - Just update count to $value',
+        condition: () => count % 2 == 0);
+    debounceWorker =
+        debounce<String>(inputText, (value) => debounceKeywordList.add(value), time: Duration(seconds: 2));
+    intervalWorker =
+        interval<String>(inputText, (value) => intervalKeywordList.add(value), time: Duration(seconds: 2));
+    inputController = TextEditingController();
     super.onInit();
   }
 
   @override
   void onClose() {
+    onceWorker.dispose();
+    everWorker.dispose();
+    debounceWorker.dispose();
+    intervalWorker.dispose();
+    inputController.dispose();
     super.onClose();
   }
 
   void increment() => count.value++;
   void decrease() => count.value--;
+  void updateText() {
+    inputText.value = inputController.text;
+  }
 }
--- lib/pages/home/views/home_view.dart
+++ lib/pages/home/views/home_view.dart
@@ -14,14 +14,69 @@ class HomeView extends GetView<HomeController> {
         title: const Text('GetX Home'),
         centerTitle: true,
       ),
-      body: Center(
-        child: Column(mainAxisSize: MainAxisSize.min, children: [
-          Obx(() => Text(controller.count.toString())),
-          const SizedBox(height: 8),
-          ElevatedButton(
-              onPressed: controller.increment,
-              child: const Text('Increase')),
-        ]),
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Center(
+          child: Column(mainAxisSize: MainAxisSize.min, children: [
+            Obx(() => Text(controller.count.value.toString())),
+            const SizedBox(height: 8),
+            Obx(() => Text(controller.onceMessage.value)),
+            Obx(() => Text(controller.everMessage.value)),
+            const SizedBox(height: 8),
+            ElevatedButton(
+                onPressed: controller.increment, child: const Text('Increase')),
+            const SizedBox(height: 8),
+
+            
+            TextFormField(
+              controller: controller.inputController,
+              onChanged: (v) => controller.updateText(),
+              decoration: const InputDecoration(hintText: 'Enter the text'),
+            ),
+            const SizedBox(height: 8,),
+            Expanded(
+                child: Row(
+              children: [
+                Expanded(
+                    child: Column(
+                  children: [
+                    const Text('Debouce list keyword'),
+                    const SizedBox(
+                      height: 8,
+                    ),
+                    Expanded(
+                      child: Obx(() => ListView.separated(
+                          itemBuilder: (context, index) => Padding(
+                                padding: const EdgeInsets.all(8),
+                                child: Text(controller.debounceKeywordList[index]),
+                              ),
+                          separatorBuilder: (context, index) => const Divider(),
+                          itemCount: controller.debounceKeywordList.length)),
+                    ),
+                  ],
+                )),
+                Expanded(
+                    child: Column(
+                  children: [
+                    const Text('Interval list keyword'),
+                    const SizedBox(
+                      height: 8,
+                    ),
+                    Expanded(
+                      child: Obx(() => ListView.separated(
+                          itemBuilder: (context, index) => Padding(
+                                padding: const EdgeInsets.all(8),
+                                child: Text(controller.intervalKeywordList[index]),
+                              ),
+                          separatorBuilder: (context, index) => const Divider(),
+                          itemCount: controller.intervalKeywordList.length)),
+                    ),
+                  ],
+                ))
+              ],
+            ))
+          ]),
+        ),
       ),
     );
   }
